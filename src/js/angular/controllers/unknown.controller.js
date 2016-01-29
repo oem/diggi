3b0202dbe1d3b941f34e8a5cd05e2f68b6cbc29d
Ömür Özkir <oemuer.oezkir@gmail.com>
2016-01-28 17:43:56 -0800

    adds page transitions

--- src/js/angular/controllers/unknown.controller.js
+++ src/js/angular/controllers/unknown.controller.js
@@ -2,6 +2,7 @@
   'use strict';
 
   diggi.Controllers.Unknown = function () {
+    $scope.pageClass = 'unknown-page';
   };
 
 })(window.diggi || (window.diggi = {Services: {}, Controllers: {}, Routers: {}}));
